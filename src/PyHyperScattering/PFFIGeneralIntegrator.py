added a hacky fix so that one can use splitpix and masking at the same time. needs further testing, may be some issues with restoring unwanted pixels. ideal case would be compute missing wedge again to generate a mask, since pyfai doesn't seem to store this? need to read more about if/where a missing wedge mask gets generated

src/PyHyperScattering/PFFIGeneralIntegrator.py
@@ -2,6 +2,7 @@ import xarray as xr
 import numpy as np
 import warnings
 from collections import deque
+from scipy.ndimage import label
 from pyFAI.integrator.fiber import FiberIntegrator
 from pyFAI.units import get_unit_fiber
 from pyFAI.integrator.azimuthal import AzimuthalIntegrator
@@ -97,50 +98,37 @@ class PFFIGeneralIntegrator(PFGeneralIntegrator):
 
     def _generate_secondary_mask(self, intensity: np.ndarray) -> np.ndarray:
         """
-        Generate a refined mask after pixel splitting:
-        1. Mask all pixels where intensity <= 0.
-        2. Unmask pixels whose left&right or up&down neighbors are both > 0.
-        3. Remove connected components of masked pixels with size <= 4.
+        Generate a refined mask after pixel splitting using vectorized operations:
+        1. Initial mask: intensity <= 0.
+        2. Unmask pixels with both left&right or up&down neighbors > 0.
+        3. Label connected components (4-connectivity) and remove those smaller than min_component_size.
         Returns boolean array of same shape, True for masked pixels.
         """
-        H, W = intensity.shape
+        min_component_size = 4
+
         # Step 1: initial mask
         mask = intensity <= 0
         # Step 2: unmask pixels with opposite neighbors > 0
-        to_unmask = []
-        for y in range(H):
-            for x in range(W):
-                if mask[y, x]:
-                    # left & right
-                    if x-1 >= 0 and x+1 < W and intensity[y, x-1] > 0 and intensity[y, x+1] > 0:
-                        to_unmask.append((y, x))
-                        continue
-                    # up & down
-                    if y-1 >= 0 and y+1 < H and intensity[y-1, x] > 0 and intensity[y+1, x] > 0:
-                        to_unmask.append((y, x))
-        for y, x in to_unmask:
-            mask[y, x] = False
-        # Step 3: remove small components (<=4)
-        visited = np.zeros((H, W), dtype=bool)
-        for i in range(H):
-            for j in range(W):
-                if mask[i, j] and not visited[i, j]:
-                    # BFS component
-                    queue = deque([(i, j)])
-                    comp = []
-                    visited[i, j] = True
-                    while queue:
-                        y, x = queue.popleft()
-                        comp.append((y, x))
-                        for dy, dx in ((1,0),(-1,0),(0,1),(0,-1)):
-                            ny, nx = y+dy, x+dx
-                            if 0 <= ny < H and 0 <= nx < W and mask[ny, nx] and not visited[ny, nx]:
-                                visited[ny, nx] = True
-                                queue.append((ny, nx))
-                    if len(comp) <= 4:
-                        # unmask small component
-                        for y, x in comp:
-                            mask[y, x] = False
+        pos = intensity > 0
+        # left-right
+        lr_unmask = np.zeros_like(mask)
+        lr_unmask[:,1:-1] = pos[:, :-2] & pos[:, 2:]
+        # up-down
+        ud_unmask = np.zeros_like(mask)
+        ud_unmask[1:-1,:] = pos[:-2, :] & pos[2:, :]
+        # apply unmask
+        mask &= ~(lr_unmask | ud_unmask)
+
+        # Step 3: remove small components via scipy.ndimage.label
+        structure = np.array([[0,1,0],[1,1,1],[0,1,0]], dtype=int)
+        labeled, num = label(mask, structure=structure)
+        if num > 0:
+            counts = np.bincount(labeled.ravel())
+            # find labels to remove (size < threshold)
+            small = np.where(counts < min_component_size)[0]
+            if small.size > 0:
+                remove = np.isin(labeled, small)
+                mask[remove] = False
         return mask
     
     def integrateSingleImage(self, da: xr.DataArray) -> xr.DataArray:
@@ -189,7 +177,8 @@ class PFFIGeneralIntegrator(PFGeneralIntegrator):
         print(f"Incidence Angle (rad): {self.incident_angle}")
         method = ("bbox", "csr", "cython") if self.split_pixels else 'no'
 
-            # if splitting, get base integration to build secondary mask
+        ## NOTE: This is a hacky fix to pyFAI filling in the missing wedge with pixel splitting. 
+        # if splitting, get base integration to build secondary mask
         if self.split_pixels:
             base = self.integrator.integrate2d_grazing_incidence(
                 data=img, unit_ip="qip_A^-1", unit_oop="qoop_A^-1",
@@ -199,6 +188,7 @@ class PFFIGeneralIntegrator(PFGeneralIntegrator):
                 method='no'
             )
             sec_mask = self._generate_secondary_mask(base.intensity)
+            print ('(PyHyper) WARNING: This is a hacky-fix to use splitpix with pyFAI so the missing wedge does not show up. If you are using this, you should understand reciprocal space corrections. Also, you should know that this is not perfect, it should be replaced with the actual wedge calculation to generate the mask.')
         else:
             sec_mask = None
 
@@ -208,12 +198,14 @@ class PFFIGeneralIntegrator(PFGeneralIntegrator):
             unit_oop="qoop_A^-1",
             npt_ip=self.npt_ip,
             npt_oop=self.npt_oop,
-            mask=self.mask,
+            # mask=self.mask,
+            mask=None,
             sample_orientation=self.sample_orientation,
             incident_angle=self.incident_angle,
             tilt_angle=self.tilt_angle,
             method=method
         )
+        
 
         data = result.intensity
         if sec_mask is not None: