bd949af58210ffbaa86a7ff5f928164c86b71232
kewh5868 <kewh5868@colorado.edu>
2025-05-12 14:01:26 -0600

    updating PFFI integrateSingleImage to shape to pix_x pix_y dimensions and default to detector dimensions if user does not reshape

--- src/PyHyperScattering/PFFIGeneralIntegrator.py
+++ src/PyHyperScattering/PFFIGeneralIntegrator.py
@@ -23,8 +23,8 @@ class PFFIGeneralIntegrator(PFGeneralIntegrator):
         sample_orientation: int = 1,
         incident_angle: float = 0.12,
         tilt_angle: float = 0.0,
-        npt_ip: int = 500,
-        npt_oop: int = 500,
+        npt_ip: int = None,
+        npt_oop: int = None,
         **kwargs
     ):
         """
@@ -97,6 +97,18 @@ class PFFIGeneralIntegrator(PFGeneralIntegrator):
         else:
             stack_dim, coords = None, None
 
+        # Before calling integrate2d_grazing_incidence:
+        if self.npt_ip is None:
+            try:
+                self.npt_ip = da.sizes["pix_x"]
+            except KeyError:
+                raise ValueError("No 'pix_x' dimension found on DataArray to infer npt_ip")
+        if self.npt_oop is None:
+            try:
+                self.npt_oop = da.sizes["pix_y"]
+            except KeyError:
+                raise ValueError("No 'pix_y' dimension found on DataArray to infer npt_oop")
+
         result = self.integrator.integrate2d_grazing_incidence(
             data=img,
             unit_ip="qip_A^-1",
@@ -118,131 +130,132 @@ class PFFIGeneralIntegrator(PFGeneralIntegrator):
             },
             attrs=da.attrs
         )
-        
+
         if stack_dim:
             out_da = out_da.expand_dims({stack_dim: len(coords)}) \
                            .assign_coords({stack_dim: coords})
 
         return out_da
 
-    # def integrate1d(self, da: xr.DataArray, vertical_integration: bool = True) -> xr.DataArray:
-    #     """
-    #     Perform 1D grazing-incidence integration (line cut).
+    ### Methods to potentially add into the integrateSingleImage workflow provided user options for PFFI integrator.
+    def integrate1d(self, da: xr.DataArray, vertical_integration: bool = True) -> xr.DataArray:
+        """
+        Perform 1D grazing-incidence integration (line cut).
 
-    #     Chooses between q_ip (vertical integration) or q_oop (horizontal integration).
+        Chooses between q_ip (vertical integration) or q_oop (horizontal integration).
 
-    #     Parameters
-    #     ----------
-    #     da : xarray.DataArray
-    #         Input image or 2D integrated DataArray.
-    #     vertical_integration : bool, default True
-    #         If True, integrate along q_oop to get intensity vs q_ip; else along q_ip to get vs q_oop.
+        Parameters
+        ----------
+        da : xarray.DataArray
+            Input image or 2D integrated DataArray.
+        vertical_integration : bool, default True
+            If True, integrate along q_oop to get intensity vs q_ip; else along q_ip to get vs q_oop.
 
-    #     Returns
-    #     -------
-    #     xarray.DataArray
-    #         1D intensity vs 'qip' or 'qoop' with units '1/Å'.
-    #     """
-    #     img = np.squeeze(da.values)
-    #     result = self.integrator.integrate1d_grazing_incidence(
-    #         data=img,
-    #         unit_ip="qip_A^-1",
-    #         unit_oop="qoop_A^-1",
-    #         npt_ip=self.npt_ip,
-    #         npt_oop=self.npt_oop,
-    #         vertical_integration=vertical_integration,
-    #         mask=self.mask,
-    #         sample_orientation=self.sample_orientation,
-    #         incident_angle=self.incident_angle,
-    #         tilt_angle=self.tilt_angle
-    #     )
-    #     if vertical_integration:
-    #         axis, coord = "qip", result.ip
-    #     else:
-    #         axis, coord = "qoop", result.oop
+        Returns
+        -------
+        xarray.DataArray
+            1D intensity vs 'qip' or 'qoop' with units '1/Å'.
+        """
+        img = np.squeeze(da.values)
+        result = self.integrator.integrate1d_grazing_incidence(
+            data=img,
+            unit_ip="qip_A^-1",
+            unit_oop="qoop_A^-1",
+            npt_ip=self.npt_ip,
+            npt_oop=self.npt_oop,
+            vertical_integration=vertical_integration,
+            mask=self.mask,
+            sample_orientation=self.sample_orientation,
+            incident_angle=self.incident_angle,
+            tilt_angle=self.tilt_angle
+        )
+        if vertical_integration:
+            axis, coord = "qip", result.ip
+        else:
+            axis, coord = "qoop", result.oop
 
-    #     return xr.DataArray(
-    #         data=result.intensity,
-    #         dims=(axis,),
-    #         coords={axis: (axis, coord, {"units": "1/Å"})},
-    #         attrs=da.attrs
-    #     )
+        return xr.DataArray(
+            data=result.intensity,
+            dims=(axis,),
+            coords={axis: (axis, coord, {"units": "1/Å"})},
+            attrs=da.attrs
+        )
 
-    # def integrate2d_exitangles(self, da: xr.DataArray) -> xr.DataArray:
-    #     """
-    #     Map intensities to detector exit angles (chi/psi).
+    def integrate2d_exitangles(self, da: xr.DataArray) -> xr.DataArray:
+        """
+        Map intensities to detector exit angles (chi/psi).
 
-    #     Provides a 2D intensity map of vertical vs horizontal exit angles in radians.
+        Provides a 2D intensity map of vertical vs horizontal exit angles in radians.
 
-    #     Parameters
-    #     ----------
-    #     da : xarray.DataArray
-    #         Raw detector image.
+        Parameters
+        ----------
+        da : xarray.DataArray
+            Raw detector image.
 
-    #     Returns
-    #     -------
-    #     xarray.DataArray
-    #         Intensity with dims ('exit_angle_vertical','exit_angle_horizontal') and radian units.
-    #     """
-    #     img = np.squeeze(da.values)
-    #     result = self.integrator.integrate2d_exitangles(
-    #         data=img,
-    #         npt_ip=self.npt_ip,
-    #         npt_oop=self.npt_oop,
-    #         sample_orientation=self.sample_orientation,
-    #         incident_angle=self.incident_angle,
-    #         tilt_angle=self.tilt_angle,
-    #         mask=self.mask
-    #     )
+        Returns
+        -------
+        xarray.DataArray
+            Intensity with dims ('exit_angle_vertical','exit_angle_horizontal') and radian units.
+        """
+        img = np.squeeze(da.values)
+        result = self.integrator.integrate2d_exitangles(
+            data=img,
+            npt_ip=self.npt_ip,
+            npt_oop=self.npt_oop,
+            sample_orientation=self.sample_orientation,
+            incident_angle=self.incident_angle,
+            tilt_angle=self.tilt_angle,
+            mask=self.mask
+        )
 
-    #     return xr.DataArray(
-    #         data=result.intensity,
-    #         dims=("exit_angle_vertical", "exit_angle_horizontal"),
-    #         coords={
-    #             "exit_angle_vertical": ("exit_angle_vertical", result.oop, {"units": "rad"}),
-    #             "exit_angle_horizontal": ("exit_angle_horizontal", result.ip, {"units": "rad"}),
-    #         },
-    #         attrs=da.attrs
-    #     )
+        return xr.DataArray(
+            data=result.intensity,
+            dims=("exit_angle_vertical", "exit_angle_horizontal"),
+            coords={
+                "exit_angle_vertical": ("exit_angle_vertical", result.oop, {"units": "rad"}),
+                "exit_angle_horizontal": ("exit_angle_horizontal", result.ip, {"units": "rad"}),
+            },
+            attrs=da.attrs
+        )
 
-    # def integrate2d_polar(self, da: xr.DataArray, polar_degrees: bool = False) -> xr.DataArray:
-    #     """
-    #     Convert to polar coordinates: azimuthal angle vs q magnitude.
+    def integrate2d_polar(self, da: xr.DataArray, polar_degrees: bool = False) -> xr.DataArray:
+        """
+        Convert to polar coordinates: azimuthal angle vs q magnitude.
 
-    #     All q values are in 1/Å. Polar angle units default to radians.
+        All q values are in 1/Å. Polar angle units default to radians.
 
-    #     Parameters
-    #     ----------
-    #     da : xarray.DataArray
-    #         Raw detector image.
-    #     polar_degrees : bool, default False
-    #         If True, polar angle coords are in degrees.
+        Parameters
+        ----------
+        da : xarray.DataArray
+            Raw detector image.
+        polar_degrees : bool, default False
+            If True, polar angle coords are in degrees.
 
-    #     Returns
-    #     -------
-    #     xarray.DataArray
-    #         Intensity with dims ('polar_angle','q_mod') and units metadata.
-    #     """
-    #     img = np.squeeze(da.values)
-    #     result = self.integrator.integrate2d_polar(
-    #         data=img,
-    #         sample_orientation=self.sample_orientation,
-    #         incident_angle=self.incident_angle,
-    #         tilt_angle=self.tilt_angle,
-    #         polar_degrees=polar_degrees,
-    #         radial_unit="A^-1",
-    #         mask=self.mask
-    #     )
-    #     angle_units = "deg" if polar_degrees else "rad"
-    #     return xr.DataArray(
-    #         data=result.intensity,
-    #         dims=("polar_angle", "q_mod"),
-    #         coords={
-    #             "polar_angle": ("polar_angle", result.azimuthal, {"units": angle_units}),
-    #             "q_mod": ("q_mod", result.radial, {"units": "1/Å"}),
-    #         },
-    #         attrs=da.attrs
-    #     )
+        Returns
+        -------
+        xarray.DataArray
+            Intensity with dims ('polar_angle','q_mod') and units metadata.
+        """
+        img = np.squeeze(da.values)
+        result = self.integrator.integrate2d_polar(
+            data=img,
+            sample_orientation=self.sample_orientation,
+            incident_angle=self.incident_angle,
+            tilt_angle=self.tilt_angle,
+            polar_degrees=polar_degrees,
+            radial_unit="A^-1",
+            mask=self.mask
+        )
+        angle_units = "deg" if polar_degrees else "rad"
+        return xr.DataArray(
+            data=result.intensity,
+            dims=("polar_angle", "q_mod"),
+            coords={
+                "polar_angle": ("polar_angle", result.azimuthal, {"units": angle_units}),
+                "q_mod": ("q_mod", result.radial, {"units": "1/Å"}),
+            },
+            attrs=da.attrs
+        )
 
     def __str__(self) -> str:
         return (
